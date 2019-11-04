ce1c196fb56777272e202c868a2587a57857ad7f
nilinswap <nilinswap@gmail.com>
2019-11-04 13:00:45 +0530

    MG 2; test yet to write

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ for all utility functions
 """
 from typing import List, Optional
 import random
+random.seed(0)
 
 class Cord:
 	"""
@@ -55,10 +56,28 @@ def get_random_map(map: List[Block] ,width: int, length: int, level: int) -> Lis
 		rb = Cord(width-2, length - 2)
 		parent_block = Block(lt, rb)
 		map.append(parent_block)
-		map = get_random_map(map, width, length, level - 1)
+		map = get_random_map(map = map, width =  width, length= length, level = level - 1)
 		return map
+	
+	slaughter_block = random.choice(map)
+	s_length = slaughter_block.rb.x - slaughter_block.lt.x + 1
+	s_width = slaughter_block.rb.y - slaughter_block.lt.y + 1
+	if s_length < 5 or s_width < 5:
+		return map
+	cut_point_x = slaughter_block.lt.x + random.randint(2, s_length - 2)
+	cut_point_y = slaughter_block.lt.y + random.randint(2, s_width - 2)
+	
+	child_block = Block(Cord(cut_point_x+1, cut_point_y+1), slaughter_block.rb)
+	slaughter_block.rb = Cord(cut_point_x-1, cut_point_y-1)
+	print("slaughter_block", slaughter_block.rb)
+	map.append(child_block)
+	
+	map = get_random_map(map = map, length = length, width = width, level= level-1)
+	
 	
 	
 	
 	
 	return map
+
+def test():
